d132d7c7fdb10672fa0f7478a0d96defaae33172
Polina Drokina <sukhova2016@inbox.ru>
2022-10-31 19:29:29 +0400

    Добавлено решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите день недели: ");
+string days = Console.ReadLine();
+
+if (days.ToLower() == "6" || days.ToLower() == "7")
+{
+    Console.WriteLine("День является выходным");
+}
+
+if (days.ToLower() == "1" || days.ToLower() == "2"|| days.ToLower() == "3"|| days.ToLower() == "4"|| days.ToLower() == "5")
+{
+    Console.WriteLine("День не является выходным");
+}
