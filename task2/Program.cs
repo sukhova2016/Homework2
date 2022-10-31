b9205bcb15075f7e6267f27c788a03516ff1438f
Polina Drokina <sukhova2016@inbox.ru>
2022-10-31 16:37:29 +0400

    Добавлено решение второй задачи

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Number = 32679;
+
+if (Number / 10 < 10)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    while (Number / 10 > 99)
+    {
+        Number = Number / 10;
+    }
+    Console.WriteLine(Number % 10);
+}
